3dac93b12bbea7c5ac34d35ddd7df298e3494acf
unknown <salman.khalid@laptop-096.Cdocs.local>
2017-10-14 00:15:39 +0500

    Basic code structure

---
+++ TopShelfTestingSolution/TopShelfTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using Topshelf;
+
+namespace TopShelfTest
+{
+
+    
+    public class TownCrier
+    {
+        readonly Timer _timer;
+        public TownCrier()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} and all is well.",DateTime.Now);
+        }
+
+        public void Start() { _timer.Start(); }
+        public void Stop() { _timer.Stop(); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run( x=> {
+                x.Service<TownCrier>(s =>                        //2
+                {
+                    s.ConstructUsing(name => new TownCrier());     //3
+                    s.WhenStarted(tc => tc.Start());              //4
+                    s.WhenStopped(tc => tc.Stop());               //5
+                });
+                x.RunAsLocalSystem();                            //6
+
+                x.SetDescription("Sample Topshelf Host");        //7
+                x.SetDisplayName("Stuff");                       //8
+                x.SetServiceName("Stuff");
+            });
+        }
+    }
+}
